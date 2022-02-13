ab56b51cf8aba44b6e88d32eeb4a843e7e4edb02
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-13 09:43:17 +0600

    Add some function

--- app.js
+++ app.js
@@ -17,26 +17,46 @@ function getCurrentQuantity(fieldId, currentTotalPriceFieldId, initialPrice, isP
     }else{
         alert("Please don't click on minus when your quantity is 0!!!!!");
     }
+
+    getTotalPrice();
 }
 
 function getIndividualTotalPrice(current_Total_Price_field, initialPrice, newQuantity){
-    const current_Total_Price = newQuantity * initialPrice;
-    current_Total_Price_field.innerText = current_Total_Price;
+    const new_Total_Price = newQuantity * initialPrice;
+    current_Total_Price_field.innerText = new_Total_Price;
+}
+
+function getInputValue(fieldId){
+    const field = document.getElementById(fieldId);
+    const fieldValue = parseInt(field.value);
+    return fieldValue;
+}
+
+function getTotalPrice(){
+    const phoneTotalPrice = getInputValue('phone_quantity_field') * 1000;
+    const caseTotalPrice = getInputValue('case_quantity_field') * 50;
+    const subTotalField = document.getElementById('sub_total');
+    const subTotalPrice = phoneTotalPrice + caseTotalPrice;
+    subTotalField.innerText = subTotalPrice;
+
+    const taxField = document.getElementById('tax');
+    const tax = subTotalPrice / 10;
+    taxField.innerText = tax;
 }
 
 // //--Phone Increment and Decrement Button--//
 document.getElementById('phone_plus_btn').addEventListener('click', function () {
-    getCurrentQuantity('phone_quantity_field', 'total_phone_price', 1219, true);
+    getCurrentQuantity('phone_quantity_field', 'total_phone_price', 1000, true);
 });
 document.getElementById('phone_minus_btn').addEventListener('click', function () {
-    getCurrentQuantity('phone_quantity_field', 'total_phone_price', 1219, false);
+    getCurrentQuantity('phone_quantity_field', 'total_phone_price', 1000, false);
 });
 
 // //--Case Increment and Decrement Button--//
 document.getElementById('case_plus_btn').addEventListener('click', function () {
-    getCurrentQuantity('case_quantity_field', 'total_case_price', 59, true);
+    getCurrentQuantity('case_quantity_field', 'total_case_price', 50, true);
 });
 document.getElementById('case_minus_btn').addEventListener('click', function () {
-    getCurrentQuantity('case_quantity_field', 'total_case_price', 59, false);
+    getCurrentQuantity('case_quantity_field', 'total_case_price', 50, false);
 });
 
